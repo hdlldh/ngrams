ad38af5d39df6d73445f92af13359bc306f17c31
Donglin Hu <hdlldh@gmail.com>
2021-11-27 23:43:03 -0800

    change run script

--- script/run_ngram_builder.sh
+++ script/run_ngram_builder.sh
@@ -1,13 +1,14 @@
-$SPARK_HOME/bin/spark-submit --master local[*] \
+$SPARK_HOME/bin/spark-submit \
+--master local[*] \
 --deploy-mode client \
 --class NGramBuilder \
---name NGramBuilderOnSpark \
---driver-memory 1024M \
+--name NGramBuilderApp \
+--driver-memory 3G \
 --driver-cores 1 \
---executor-memory 1G \
-$HOME/IdeaProjects/ngrams/target/scala-2.12/ngrams_2.12-0.1.jar \
-10 \
-$HOME/PycharmProjects/wiki_scala/wiki_sentences_samples.dat.gz \
+--executor-memory 12G \
+$HOME/Workspace/ngrams/target/scala-2.12/ngrams_2.12-0.1.jar \
+60000 \
+$HOME/Workspace/wiki/wiki_sentences_20201201.dat.gz \
 $HOME/tmp/top_n_vocab \
 $HOME/tmp/bigram_counts \
 $HOME/tmp/trigram_counts
